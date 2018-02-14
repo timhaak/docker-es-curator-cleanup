86a21c679fffd46a31927e24d65bae534a18486b
Tim Haak <tim@haak.co>
2018-02-14 15:35:39 +0200

    Fix timeouts

--- files/startWorker.sh
+++ files/startWorker.sh
@@ -12,6 +12,20 @@ export LOG_LEVEL
 export REDIS_PORT
 export REDIS_HOST
 export REDIS_DB
+export WORKER_TIMEOUT
+export WORKER_RESULT_TIMEOUT
+export WORKER_LOGGING_LEVEL
 
-echo rq worker --url redis://${REDIS_HOST}:${REDIS_PORT}/${REDIS_DB}
-rq worker --url redis://${REDIS_HOST}:${REDIS_PORT}/${REDIS_DB}
+echo rq worker \
+  -v \
+  --url redis://${REDIS_HOST}:${REDIS_PORT}/${REDIS_DB} \
+  --worker-ttl ${WORKER_TIMEOUT} \
+  --results-ttl ${WORKER_RESULT_TIMEOUT} \
+  --logging_level ${WORKER_LOGGING_LEVEL}
+
+  rq worker \
+    -v \
+    --url redis://${REDIS_HOST}:${REDIS_PORT}/${REDIS_DB} \
+    --worker-ttl ${WORKER_TIMEOUT} \
+    --results-ttl ${WORKER_RESULT_TIMEOUT} \
+    --logging_level ${WORKER_LOGGING_LEVEL}
