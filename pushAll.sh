74ca10cd0afe1d95646215b3ad0b7cf0caeb6e13
Tim Haak <tim@haak.co>
2018-02-14 15:06:01 +0200

    Adde docker for worker

--- pushAll.sh
+++ pushAll.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 docker push artifactory.mmih.biz:5000/es-curator-base
 docker push artifactory.mmih.biz:5000/es-curator-cleanup
+docker push artifactory.mmih.biz:5000/es-curator-cleanup-worker
